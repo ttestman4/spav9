2cc4766e7654538c66d12142afe2d0d42eddef1d
Test Man <ttestman4@gmail.com>
2020-03-10 19:49:12 +0530

    Add form on condition

--- projects/spa/src/app/form-test/form-test.component.ts
+++ projects/spa/src/app/form-test/form-test.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+
+interface tabData {
+  name: string;
+  enabled: boolean;
+  frmCtrl: AbstractControl
+}
 
 @Component({
   selector: 'spa-form-test',
@@ -6,10 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-test.component.scss']
 })
 export class FormTestComponent implements OnInit {
-
-  constructor() { }
+  personForm: FormGroup;
+  resEnabled = true;
+  resForm: AbstractControl;
+  offEnabled = true;
+  gymEnabled = true;
+  constructor(private fb: FormBuilder) {
+    this.personForm = this.fb.group({
+      core: this.fb.group({
+        name: ['', Validators.required],
+      }),
+      residential: this.fb.group({
+        resAdd: ['', Validators.required]
+      }),
+      office: this.fb.group({
+        offAdd: ['', Validators.required]
+      }),
+      gym: this.fb.group({
+        gymAdd: ['', Validators.required]
+      })
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  updateForm() {
+    this.resEnabled = !this.resEnabled;
+    if (this.resEnabled) {
+      this.personForm.addControl('residential', this.resForm);
+    } else {
+      this.resForm = this.personForm.get('residential');
+      this.personForm.removeControl('residential');
+    }
   }
 
 }
--- projects/spa/src/app/form-test/form-test.module.ts
+++ projects/spa/src/app/form-test/form-test.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { FormTestRoutingModule } from './form-test-routing.module';
 import { FormTestComponent } from './form-test.component';
@@ -9,7 +15,13 @@ import { FormTestComponent } from './form-test.component';
   declarations: [FormTestComponent],
   imports: [
     CommonModule,
-    FormTestRoutingModule
+    FormTestRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
   ]
 })
 export class FormTestModule { }
